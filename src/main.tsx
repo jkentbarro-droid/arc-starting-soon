fafb45c45b03db41074f5eaa4e2005c38e04d6eb
jkentbarro-droid <jkentbarro@gmail.com>
2025-10-26 23:44:48 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ArcRaidersStartingSoon from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ArcRaidersStartingSoon />
+  </React.StrictMode>
+)
 
