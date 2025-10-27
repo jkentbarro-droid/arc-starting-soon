bed29aa8bc1dc11c30015d3119c950c108b9f75c
jkentbarro-droid <jkentbarro@gmail.com>
2025-10-26 23:42:39 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
   plugins: [],
-}
+};
